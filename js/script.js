e2b7ffac6bdec35754814b68e6c4e93b9e5f5bf6
Taylor Halsted <taylor.halsted@gmail.com>
2017-01-18 12:35:54 -0500

    update hs demo

--- js/script.js
+++ js/script.js
@@ -5,115 +5,164 @@ $(document).ready(function(){
     // specifies Headspace demo page
 
     var randBetween = function(min,max) {
-        var ans = Math.floor(Math.random()*(max-min+1))+min;
-        return ans;
+      var ans = Math.floor(Math.random()*(max-min+1))+min;
+      return ans;
+    }
+
+    function shuffler(x) {
+      var a = [];
+      while(a.length<x){
+        var ans = Math.round(Math.random()*(x-1));
+        if (a.indexOf(ans) === -1) {
+          a.push(ans);
+        }
       }
+      return a;
+    }
 
     var users = {
       Alex: {
         id: 1,
         picture: "url('../img/hs-demo-alex.png')",
         age: 28,
-        level: randBetween(2,6),
+        level: randBetween(2,5),
         comments: {
-          1: "xxxx",
-          2: "xxxx",
-          3: "xxxx",
-          4: "xxxx",
-          5: "xxxx",
-          6: "xxxx",
-          7: "xxxx",
-          8: "xxxx",
-          9: "xxxx",
-          10: "xxxx"
+          1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          5: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          6: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          7: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          8: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          9: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          10: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se"
         }
       },
       George: {
         id: 2,
-        picture: "url('../img/hs-demo-george.png')",
+        picture: "url('../../img/hs-demo-george.png')",
         age: 29,
         level: 10,
         comments: {
-          1: "xxxx",
-          2: "xxxx",
-          3: "xxxx",
-          4: "xxxx",
-          5: "xxxx",
-          6: "xxxx",
-          7: "xxxx",
-          8: "xxxx",
-          9: "xxxx",
-          10: "xxxx"
+          1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          5: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          6: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          7: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          8: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          9: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          10: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se"
         }
       },
       Emily: {
         id: 3,
-        picture: "url('../img/hs-demo-emily.png')",
+        picture: "url('../../img/hs-demo-emily.png')",
         age: 27,
         level: 10,
         comments: {
-          1: "xxxx",
-          2: "xxxx",
-          3: "xxxx",
-          4: "xxxx",
-          5: "xxxx",
-          6: "xxxx",
-          7: "xxxx",
-          8: "xxxx",
-          9: "xxxx",
-          10: "xxxx"
+          1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          5: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          6: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          7: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          8: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          9: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          10: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se"
         }
       },
       PJ: {
         id: 4,
-        picture: "url('../img/hs-demo-pj.png')",
+        picture: "url('../../img/hs-demo-pj.png')",
         age: 26,
         level: 10,
         comments: {
-          1: "xxxx",
-          2: "xxxx",
-          3: "xxxx",
-          4: "xxxx",
-          5: "xxxx",
-          6: "xxxx",
-          7: "xxxx",
-          8: "xxxx",
-          9: "xxxx",
-          10: "xxxx"
+          1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          5: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          6: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          7: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          8: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          9: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          10: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se"
         }
       },
       Julia: {
         id: 5,
-        picture: "url('../img/hs-demo-julia.png')",
+        picture: "url('../../img/hs-demo-julia.png')",
         age: 23,
         level: 10,
         comments: {
-          1: "xxxx",
-          2: "xxxx",
-          3: "xxxx",
-          4: "xxxx",
-          5: "xxxx",
-          6: "xxxx",
-          7: "xxxx",
-          8: "xxxx",
-          9: "xxxx",
-          10: "xxxx"
+          1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          3: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          4: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          5: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          6: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          7: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          8: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          9: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se",
+          10: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipisicing elit, se"
         }
       }
     }
 
-    for(var i = 1; i <= 10; i++) {
-      $circle = $("<div class='circler' data-id="+i+" style='margin: 172px 0 0 "+((i-1)*69+10)+"px'><h1>"+i+"</h1></div>")
+    var usernames = ["George","Emily","PJ","Julia"]
 
-      if(i <= users["Alex"]["level"]) {
-        $circle.css('background-color', '#CBAB6C');
+    var trackerMaker = function() {
+      for(var i = 1; i <= 10; i++) {
+        $circle = $("<div class='circler' data-id="+i+" style='margin: 221px 0 0 "+((i-1)*69+10)+"px'><h1>"+i+"</h1></div>")
+        if(i <= users["Alex"]["level"]) {
+          $circle.css('background-color', '#CBAB6C');
+        }
+        $('#tracker').append($circle)
       }
-
-      $('#tracker').append($circle)
-
     }
 
+    var snippetMaker = function() {
+      trackerMaker();
+      var order = shuffler(4);
 
+      for(var i = 1; i <= 2; i++) {
+        var name = usernames[order[i-1]];
+        var age = users[name]["age"];
+        var curLevel = users["Alex"]["level"];
+        var imgUrl = users[name]["picture"];
+        var aComm = users["Alex"]["comments"][curLevel];
+
+        if(i === 1) {
+          var snip = users[name]["comments"][curLevel]
+        } else {
+          var snip = users[name]["comments"][randBetween(curLevel+1,10)]
+        }
+
+        $('.snippet-header').eq(i-1).text(name+", "+age)
+        $('.snippet p').eq(i-1).text(snip)
+        $('#snippet-pic'+(i)).css('background-image', imgUrl);
+      }
+
+      $('.snippet p').eq(2).empty();
+
+      (function myLoop (i,x) {
+        setTimeout(function () {
+          $('.snippet p').eq(2).append(aComm[i]);
+          if ((i+1) < x) {
+            myLoop((i+1),x)
+          } else {
+            users["Alex"]["level"]++;
+            snippetMaker();
+          }
+        }, 25)
+      })(0,aComm.length);
+    }
+
+    snippetMaker();
   }
 
 
